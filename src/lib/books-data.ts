86cfe7266595088f76233586e24d4fcf63c7be45
contigen <69436315+contigen@users.noreply.github.com>
2023-08-27 04:32:51 +0100

    Add fake book data

---
+++ src/lib/books-data.ts
@@ -0,0 +1,52 @@
+export const fakeBookData = [
+  {
+    title: 'Whispers of Serenity',
+    author: 'Sana Malik',
+    genre: 'Romance',
+  },
+  {
+    title: 'Chronicles of Epsilon',
+    author: 'Alexei Petrov',
+    genre: 'Science Fiction',
+  },
+  {
+    title: "Harmony's Echo",
+    author: 'Leila Chen',
+    genre: 'Fantasy',
+  },
+  {
+    title: 'Threads of Destiny',
+    author: 'Kwame Adjei',
+    genre: 'Historical Fiction',
+  },
+  {
+    title: 'Mystic Melodies',
+    author: 'Aarav Patel',
+    genre: 'Mystery',
+  },
+  {
+    title: 'Journey to Lumina',
+    author: 'Isabella Costa',
+    genre: 'Adventure',
+  },
+  {
+    title: 'Soulful Whispers',
+    author: 'Amara Nwosu',
+    genre: 'Poetry',
+  },
+  {
+    title: 'Cipher Enigma',
+    author: 'Yusuf Rahman',
+    genre: 'Thriller',
+  },
+  {
+    title: 'Beneath the Stars',
+    author: 'Elena Diaz',
+    genre: 'Young Adult',
+  },
+  {
+    title: 'Legacy of Legends',
+    author: 'Malik Khan',
+    genre: 'Historical Fantasy',
+  },
+]
