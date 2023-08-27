fefae301655a7a6c728ce19d5b3b94c718427d27
contigen <69436315+contigen@users.noreply.github.com>
2023-08-27 04:32:07 +0100

    Add route handlers to manage DB data

---
+++ src/app/api/books/route.ts
@@ -0,0 +1,31 @@
+import { prisma } from '@/lib/prisma'
+import { NextResponse } from 'next/server'
+
+type BookData = {
+  title: string
+  genre: string
+  author: string
+}
+export function GET(req: Request) {
+  return new Response(`OK`)
+}
+
+export async function POST(req: Request) {
+  const body: BookData = await req.json()
+  const { title, genre, author } = body
+  try {
+    const bookEntry = await prisma.bookSugestion.create({
+      data: {
+        bookTitle: title,
+        bookGenre: genre,
+        bookAuthor: author,
+      },
+    })
+    return NextResponse.json(bookEntry)
+  } catch (err) {
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
+      { status: 500 }
+    )
+  }
+}
