6a31adb89f616db10e50a250114eff10c2364b4c
contigen <69436315+contigen@users.noreply.github.com>
2023-08-28 19:17:08 +0100

    Fix font rendering
    The intended font wasn't  displaying, adding 'display:swap' fixes it

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'], display: 'swap' })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang='en'>
+    <html lang='en' className='tracking-tighter'>
       <body className={inter.className}>{children}</body>
     </html>
   )
