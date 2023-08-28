99e2a8635a9c02f631e0de3d660958c4c2671c7e
contigen <69436315+contigen@users.noreply.github.com>
2023-08-28 19:14:45 +0100

    Refactor code

--- src/app/form.tsx
+++ src/app/form.tsx
@@ -1,4 +1,10 @@
-export function Form() {
+'use client'
+
+import { addBook } from '@/lib/db'
+import { SubmitButton } from './button'
+import { useState } from 'react'
+
+export function FormWrapper() {
   return (
     <div className='isolate bg-white px-6 py-24 sm:py-32 lg:px-8'>
       <div
@@ -14,7 +20,7 @@ export function Form() {
         />
       </div>
       <div className='mx-auto max-w-2xl text-center'>
-        <h1 className='text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
+        <h1 className='text-4xl lg:text-5xl font-bold text-gray-900 sm:text-4xl'>
           Book Suggestions
         </h1>
         <p className='mt-2 text-lg leading-8 text-gray-600'>
@@ -25,72 +31,79 @@ export function Form() {
           recommendations from people who share your tastes.
         </p>
       </div>
-      <form className='mx-auto mt-16 max-w-xl sm:mt-20'>
-        <div className='grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2'>
-          <div>
-            <label
-              htmlFor='book-title'
-              className='block text-sm font-semibold leading-6 text-gray-900'
-            >
-              Book title
-            </label>
-            <div className='mt-2.5'>
-              <input
-                type='text'
-                name='book-title'
-                id='book-title'
-                autoComplete='on'
-                required
-                className='block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
-              />
-            </div>
-          </div>
-          <div>
-            <label
-              htmlFor='genre'
-              className='block text-sm font-semibold leading-6 text-gray-900'
-            >
-              Genre
-            </label>
-            <div className='mt-2.5'>
-              <input
-                type='text'
-                name='genre'
-                id='genre'
-                autoComplete='on'
-                required
-                className='block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
-              />
-            </div>
-          </div>
-          <div className='sm:col-span-2'>
-            <label
-              htmlFor='author'
-              className='block text-sm font-semibold leading-6 text-gray-900'
-            >
-              Author&apos;s name
-            </label>
-            <div className='mt-2.5'>
-              <input
-                type='text'
-                name='author'
-                id='author'
-                autoComplete='name'
-                required
-                className='block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
-              />
-            </div>
-          </div>
-        </div>
-        <div className='mt-10'>
-          <button
-            type='submit'
-            className='block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
-          >
-            Let&apos;s talk
-          </button>
-        </div>
-      </form>
+      <Form />
     </div>
+  )
+}
+
+function Form() {
+  const [message, setMessage] = useState(``)
+  async function action(formData: FormData) {
+    const bookDataRes = await addBook(formData)
+    setMessage(bookDataRes.message)
+  }
+  return (
+    <form className='mx-auto mt-16 max-w-xl sm:mt-20' action={action}>
+      <div className='grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2'>
+        <div>
+          <label
+            htmlFor='title'
+            className='block text-sm font-semibold leading-6 text-gray-900'
+          >
+            Book title
+          </label>
+          <div className='mt-2.5'>
+            <input
+              type='text'
+              name='title'
+              id='title'
+              autoComplete='on'
+              required
+              className='block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+            />
+          </div>
+        </div>
+        <div>
+          <label
+            htmlFor='genre'
+            className='block text-sm font-semibold leading-6 text-gray-900'
+          >
+            Genre
+          </label>
+          <div className='mt-2.5'>
+            <input
+              type='text'
+              name='genre'
+              id='genre'
+              autoComplete='on'
+              required
+              className='block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+            />
+          </div>
+        </div>
+        <div className='sm:col-span-2'>
+          <label
+            htmlFor='author'
+            className='block text-sm font-semibold leading-6 text-gray-900'
+          >
+            Author&apos;s name
+          </label>
+          <div className='mt-2.5'>
+            <input
+              type='text'
+              name='author'
+              id='author'
+              autoComplete='name'
+              required
+              className='block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+            />
+          </div>
+        </div>
+      </div>
+      <div className='mt-10'>
+        <SubmitButton />
+        <p className='text-center my-4'>{message}</p>
+      </div>
+    </form>
   )
 }
